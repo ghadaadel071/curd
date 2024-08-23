669e233398333161a77eb970aa33dec5d9159c58
ghada adel <ghadanada071@gmail.com>
2024-08-23 21:56:38 +0300

    Add files via upload

---
+++ del.php
@@ -0,0 +1,10 @@
+<?php
+include "db.php";
+$id = $_GET["id"];
+$sql = "DELETE FROM curd WHERE id = $id";
+$result = mysqli_query($conn, $sql);
+
+if ($result) {
+  header("Location: index.php?msg=Data deleted successfully");
+} else {
+  echo "Failed: " . mysqli_error($conn);}
